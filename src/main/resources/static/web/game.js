3834728bd3dc5c41c6d130fdad8e4bb544ea46ae
zvonimir <zvonimir.planinic@gmail.com>
2018-02-07 13:23:16 +0100

    Finished task 4, implemented salvo

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,31 +1,32 @@
-function rowCreate() {
+function rowCreate(divId, rowId) {
 
     for (var rows = 0; rows < 11; rows++) {
-        $("#gridTable").append("<div id=" + 'row' + rows + " ></div>");
+        $(divId).append("<tr id=" + rowId + rows + " ></tr>");
     }
 }
-rowCreate();
-
-function columnCreate() {
+rowCreate("#gridShipTable", "rowShip");
+rowCreate("#gridSalvoTable", "rowSalvo");
+function columnCreate(rowId) {
     var letters = ["0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
     var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
     for (var rows = 0; rows < 11; rows++, j++) {
         if(rows == 0) {
             for(var j= 0; j < 11; j++) {
-                $("#row" + rows).append("<div class='grid border border_right' " + " >" + numbers[j] + " </div>");
+                $(rowId + rows).append("<td class='grid' " + " >" + numbers[j] + " </td>");
             }
         }else {
             for (var i = 0; i < 11; i++) {
                 if(i == 0) {
-                    $("#row" + rows).append("<div  class='grid border border_right' " + " >" + letters[rows] + "</div>");
+                    $(rowId + rows).append("<td  class='grid' " + " >" + letters[rows] + "</td>");
                 }else {
-                    $("#row" + rows).append("<div id="  + letters[rows] + i + " class='grid border border_right'" + "></div>");
+                    $(rowId + rows).append("<td id="  + letters[rows] + i + " class=" + "grid" + "></td>");
                 }
             }
         }
     }
 }
-columnCreate();
+columnCreate("#rowShip");
+columnCreate("#rowSalvo");
 
 // id=" + letters[j] + rows + "
 // id=" + letters[i] + rows + "
@@ -46,7 +47,7 @@ function paramObj(search) {
 }
 paramObj(url);
 
-function loadJsonData(param) {
+function loadJsonShipData(param) {
     $.getJSON("http://localhost:8080/api/game_view/" + param, function(data) {
         var playerCompetitor;
         var playerMain;
@@ -57,18 +58,15 @@ function loadJsonData(param) {
 
 
         ships.map(function(x) {
-            console.log(x.locations);
             x.locations.map(function(y) {
-               document.getElementById(y).style.background = "blue";
+                $("#gridShipTable #" + y).css("background-color","blue")
+               // document.getElementById(y).style.background = "blue";
             });
         });
 
 
         gameplayers.map(function(x){
-            console.log(x.player.playerId);
-                console.log(x.player.playerId);
-                if(x.player.playerId == dataplayer.id) {
-                    console.log("nesto"+dataplayer.id)
+                 if(x.player.playerId == dataplayer.id) {
                     playerMain = x.player.playerName + "(you)"
                     return playerMain;
                 }else {
@@ -81,5 +79,33 @@ function loadJsonData(param) {
     });
 }
 
-loadJsonData(key_url);
+loadJsonShipData(key_url);
+
+function loadJsonSalvoData(param) {
+    $.getJSON("http://localhost:8080/api/game_view/" + param, function(data) {
+        data.salvoes.map(function(x) {
+            x.map(function(y) {
+                console.log(y.turn);
+                if(y.player_id == param) {
+                    y.locations.map(function(z) {
+                        console.log(z);
+                        $("#gridSalvoTable #" + z).append("<div class='salvoPlayer'>" + "<span class='text'>"  + y.turn + "</span>" + "</div>");
+                    })
+
+                }
+                else {
+                    y.locations.map(function(z) {
+                        console.log("nesto " + z);
+                        $("#gridShipTable #" + z).append("<div class='salvoOpponent'>" + "<span class='text'>"  + y.turn + "</span>" + "</div>");
+                    // .css({"background-color": "red", "text-align": "center"})
+                    });
+                }
+            });
+        });
+    });
+
+}
+
+loadJsonSalvoData(key_url);
+
 
