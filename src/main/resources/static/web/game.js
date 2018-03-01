1e4f07a3ca363a6ee18b2a93cc58be838c0d6f1a
zvonimir <zvonimir.planinic@gmail.com>
2018-03-01 11:09:26 +0100

    Drag and drop - horizontal way, I can still drag to the neighbour one

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,38 +1,48 @@
+rowCreate("#gridShipTable", "rowShip");
+rowCreate("#gridSalvoTable", "rowSalvo");
+
+columnCreate("#rowShip");
+columnCreate("#rowSalvo");
+
+
+var url = window.location.href;
+var key_url;
+
+paramObj(url);
+
+loadJsonData(key_url);
+
+logout();
+
+
+
 function rowCreate(divId, rowId) {
 
     for (var rows = 0; rows < 11; rows++) {
-        $(divId).append("<tr id=" + rowId + rows + " ></tr>");
+        $(divId).append("<div id=" + rowId + rows + " ></div>");
     }
 }
-rowCreate("#gridShipTable", "rowShip");
-rowCreate("#gridSalvoTable", "rowSalvo");
+
 function columnCreate(rowId) {
     var letters = ["0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
     var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
     for (var rows = 0; rows < 11; rows++, j++) {
         if(rows == 0) {
             for(var j= 0; j < 11; j++) {
-                $(rowId + rows).append("<td class='grid' " + " >" + numbers[j] + " </td>");
+                $(rowId + rows).append("<div class='grid' " + " >" + numbers[j] + " </div>");
             }
         }else {
             for (var i = 0; i < 11; i++) {
                 if(i == 0) {
-                    $(rowId + rows).append("<td  class='grid' " + " >" + letters[rows] + "</td>");
+                    $(rowId + rows).append("<div  class='grid' " + " >" + letters[rows] + "</div>");
                 }else {
-                    $(rowId + rows).append("<td id="  + letters[rows] + i + " class=" + "grid" + "></td>");
+                    $(rowId + rows).append("<div id="  + letters[rows] + i + " class=" + "grid " + "ondrop=" + "drop(event)" +" ondragover=" + "allowDrop(event)"
+                        + " data-occupied=" + "no" + " ></div>");
                 }
             }
         }
     }
 }
-columnCreate("#rowShip");
-columnCreate("#rowSalvo");
-
-// id=" + letters[j] + rows + "
-// id=" + letters[i] + rows + "
-
-var url = window.location.href;
-var key_url;
 
 function paramObj(search) {
     var obj = {};
@@ -45,29 +55,22 @@ function paramObj(search) {
     return key_url;
 
 }
-paramObj(url);
 
-function loadJsonShipData(param) {
-    $.getJSON("http://localhost:8080/api/game_view/" + param, function(data) {
+function loadJsonShipData(param, data) {
         var playerCompetitor;
         var playerMain;
         var ships = data.ship;
         var gameplayers = data.gameplayers;
-        var dataplayer = data.player
-
-
+        var dataplayer = data.player;
 
         ships.map(function(x) {
             x.locations.map(function(y) {
                 $("#gridShipTable #" + y).css("background-color","blue")
-               // document.getElementById(y).style.background = "blue";
             });
         });
 
 
         gameplayers.map(function(x){
-
-            console.log(dataplayer)
                  if(x.player["playerId"] == dataplayer.playerId) {
                     playerMain = x.player.playerName + "(you)"
                     return playerMain;
@@ -78,49 +81,121 @@ function loadJsonShipData(param) {
         });
 
         $("#title").append("<h3>" + playerMain + " vs " +  playerCompetitor + "</h3>");
-    });
+
 }
 
-loadJsonShipData(key_url);
 
-function loadJsonSalvoData(param) {
-    $.getJSON("http://localhost:8080/api/game_view/" + param, function(data) {
+
+function loadJsonSalvoData(param, data) {
         data.salvoes.map(function(x) {
             x.map(function(y) {
                 console.log(y.turn);
                 if(y.player_id == param) {
                     y.locations.map(function(z) {
-                        console.log(z);
                         $("#gridSalvoTable #" + z).append("<div class='salvoPlayer'>" + "<span class='text'>"  + y.turn + "</span>" + "</div>");
                     })
 
                 }
                 else {
                     y.locations.map(function(z) {
-                        console.log("nesto " + z);
+
                         $("#gridShipTable #" + z).append("<div class='salvoOpponent'>" + "<span class='text'>"  + y.turn + "</span>" + "</div>");
                     // .css({"background-color": "red", "text-align": "center"})
                     });
                 }
             });
         });
-    });
 
 }
-
-loadJsonSalvoData(key_url);
 
 function logout() {
     $("#logoutButton").click(function(event) {
         event.preventDefault();
         $.post("/api/logout")
             .done(function () {
-                window.location.replace("http://localhost:8080/web/games.html");
+                window.location.replace("/web/games.html");
                 console.log("Logged out");
             })
             .fail();
     });
 }
 
-logout();
+function loadJsonData(param) {
+    $.getJSON("/api/game_view/" + param, function(data_json) {
+        var data = data_json;
+        loadJsonShipData(param, data);
+        loadJsonSalvoData(param, data);
+        createShips(data);
+
+    })
+}
+
+function createShips(data) {
+
+    var allships = {"destroyer": 3,
+        "patrol_boat": 2,
+        "submarine": 3,
+        "battleship": 4,
+        "aircraft_carrier": 5}
+
+    console.log(allships);
+    for (var property in allships) {
+        $("#shipsDrag").append("<div id=" + property + allships[property] + " draggable="  + "true" +" ondragstart=" + "drag(event)"
+            + " ondrop=" + "drop(event)" + "ondragleave=" + "dragLeave(event)" + "></div>")
+        console.log(allships[property]);
+        for(var i = 0; i<allships[property]; i++) {
+            $("#" + property + allships[property]).append("<div class=" + property + i +"></div>")
+        }
+
+    }
+
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    var firstDiv = ev.target.offsetParent["id"];
+
+    $("#" + ev.target.id).find("div").each(function(index) {
+        var counter = Number(firstDiv[1]) + index;
+        $('#' + firstDiv[0] + counter).attr('data-occupied', 'no');
+    });
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var firstDiv = ev.target["id"];
+
+    $('#' + data).find("div").each(function() {
+        var datAtr = $('#' + firstDiv).data("occupied");
+        console.log(datAtr);
+        if (datAtr == 'no') {
+            ev.target.appendChild((document.getElementById(data)));
+            $('#' + data).find("div").each(function(index) {
+                var counter = Number(firstDiv[1]) + index;
+                $('#' + firstDiv[0] + counter).attr('data-occupied', 'yes');
+            });
+        }
+    });
+    //
+    // ev.target.appendChild((document.getElementById(data)));
+    //
+    // $('#' + data).find("div").each(function(index) {
+    //     var counter = Number(firstDiv[1]) + index;
+    //     $('#' + firstDiv[0] + counter).attr('data-occupied', 'yes');
+    // });
+}
+
+
+// function dragLeave(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     console.log(data);
+//     ev.target;
+//     console.log(ev.target);
+// }
 
--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -1,136 +1,132 @@
-$(function() {
-    function getColumnsHtml(row) {
-        return row.gameplayers.map(function(gameplayer) {
-            return gameplayer.player.playerName;
-        }).join(" vs ");
-    }
+loadData();
+login();
+
+// loginReload();
+logout();
+signUp();
+createGame();
+
+allGets();
+// joinButton();
+
+function getColumnsHtml(row) {
+    return row.gameplayers.map(function(gameplayer) {
+        return gameplayer.player.playerName;
+    }).join(" vs ");
+}
 
 
-    function getRowsHTMl(data) {
-        // var player = data.player;
-        var data = data.games;
-        // data.forEach(function(element) {
-        //     console.log(element);
-        // });
-        return data.map(function(row, i) {
-           return "<li id=" + "dataAttr" + i + ">" + data[i].created + " " + getColumnsHtml(row) + "</li>"
-        }).join("");
-    }
+function getRowsHTMl(data) {
+    var data = data.games;
+    return data.map(function(row, i) {
+        return "<li id=" + "dataAttr" + i + ">" + data[i].created + " " + getColumnsHtml(row) + "</li>"
+    }).join("");
+}
 
-    function renderList(data) {
-        var html = getRowsHTMl(data);
-        document.getElementById("output").innerHTML = html;
-    }
+function renderList(data) {
+    var html = getRowsHTMl(data);
+    document.getElementById("output").innerHTML = html;
+}
 
 
-    function loadData() {
-        $.get("/api/games")
-            .done(function (data) {
-                renderList(data);
-            })
-            .fail(function (jqXHR, textStatus) {
-                showOutput("Failed: " + textStatus);
-            });
-    }
-    loadData();
-});
+function loadData() {
+    $.get("/api/games")
+        .done(function (data) {
+            renderList(data);
+        })
+        .fail(function (jqXHR, textStatus) {
+            showOutput("Failed: " + textStatus);
+        });
+}
 
-function makeLeaderBoard() {
+
+function makeLeaderBoard(data) {
     var players = new Object();
     var competitor;
 
-
-    $.getJSON("http://localhost:8080/api/games", function(data) {
-        var data = data.games;
-        data.map(function(x) {
-            x.gameplayers.map(function(y) {
-                competitor = y.player.playerName;
-                players[competitor] = [{scores: 0, win: 0, loss: 0, tie: 0}];
-            });
+    var data = data.games;
+    data.map(function(x) {
+        x.gameplayers.map(function(y) {
+            competitor = y.player.playerName;
+            players[competitor] = [{scores: 0, win: 0, loss: 0, tie: 0}];
         });
-        data.map(function(x) {
-            x.gameplayers.map(function(y) {
-                competitor = y.player.playerName;
-                if(((competitor in players) == true) && (y.score != "No score")) {
-                    var score = Number(y.score);
-                    players[competitor].map(function(j) {
-                        j.scores += score
-                    });
-                }
-            });
-        });
-        data.map(function(x) {
-            x.gameplayers.map(function(y) {
-                competitor = y.player.playerName;
+    });
+    data.map(function(x) {
+        x.gameplayers.map(function(y) {
+            competitor = y.player.playerName;
+            if(((competitor in players) == true) && (y.score != "No score")) {
+                var score = Number(y.score);
                 players[competitor].map(function(j) {
-                    if(y.score == "1") {
-                        j.win += 1;
-                    } else if (y.score == "0.5") {
-                        j.tie += 1;
-                    } else if (y.score == "0") {
-                        j.loss += 1
-                    }
+                    j.scores += score
                 });
-            })
+            }
         });
-
-        var header = ["Name", "Total", "Win", "Loss", "Tie"];
-        for (var rows = 0; rows <= (Object.keys(players).length); rows++) {
-            $('#leaderBoardTable').append("<tr id=" + 'row' + rows + " ></tr>");
-                if (rows == 0) {
-                    for(var j= 0; j < 5; j++) {
-                        $('#row' + rows).append("<td>" + header[j] + "</td>");
-                    }
+    });
+    data.map(function(x) {
+        x.gameplayers.map(function(y) {
+            competitor = y.player.playerName;
+            players[competitor].map(function(j) {
+                if(y.score == "1") {
+                    j.win += 1;
+                } else if (y.score == "0.5") {
+                    j.tie += 1;
+                } else if (y.score == "0") {
+                    j.loss += 1
                 }
-        }
-
-
-        var i = 0;
-        for (key in players) {
-            players[key].map(function(z) {
-                i = i + 1;
-                $('#row' +i).append("<td>" + key + "</td>");
-                $('#row' + i).append("<td>" + z.scores + "</td>");
-                $('#row' + i).append("<td>" + z.win + "</td>");
-                $('#row' + i).append("<td>" + z.loss + "</td>");
-                $('#row' + i).append("<td>" + z.tie + "</td>");
             });
-        }
+        })
+    });
+
+    var header = ["Name", "Total", "Win", "Loss", "Tie"];
+    for (var rows = 0; rows <= (Object.keys(players).length); rows++) {
+        $('#leaderBoardTable').append("<tr id=" + 'row' + rows + " ></tr>");
+            if (rows == 0) {
+                for(var j= 0; j < 5; j++) {
+                    $('#row' + rows).append("<td>" + header[j] + "</td>");
+                }
+            }
+    }
+
+    var i = 0;
+    for (key in players) {
+        players[key].map(function(z) {
+            i = i + 1;
+            $('#row' +i).append("<td>" + key + "</td>");
+            $('#row' + i).append("<td>" + z.scores + "</td>");
+            $('#row' + i).append("<td>" + z.win + "</td>");
+            $('#row' + i).append("<td>" + z.loss + "</td>");
+            $('#row' + i).append("<td>" + z.tie + "</td>");
+        });
+    }
+}
+
+function showOutput(text) {
+    $("#playerName").text(text);
+}
+
+function login() {
+    $("#loginButton").click(function(event) {
+        event.preventDefault();
+        var form = event.target.form;
+        loginPost(form);
+
     });
 }
-makeLeaderBoard();
 
-$(function() {
-
-    function showOutput(text) {
-        $("#playerName").text(text);
-    }
-
-    function login() {
-        $("#loginButton").click(function(event) {
-            event.preventDefault();
-            var form = event.target.form;
-            loginPost(form);
-
-
+function loginPost(form) {
+    $.post("/api/login",
+        { username: form["username"].value,
+            password: form["password"].value })
+        .done(function( ) {
+            console.log("Logged in!")
+            // loadUser();
+            loginReload()
+            // allGets();
+            joinButton();
+        })
+        .fail(function() {
+            showOutput("You have to sign up first!");
         });
-    }
-
-    function loginPost(form) {
-        $.post("/api/login",
-            { username: form["username"].value,
-                password: form["password"].value })
-            .done(function( ) {
-                console.log("Logged in!")
-                // loadUser();
-                loginReload()
-                joinButton();
-
-            })
-            .fail(function() {
-                showOutput("You have to sign up first!");
-            });
-
     }
  // load user and make  REJOIN button
  //    function loadUser() {
@@ -140,118 +136,127 @@ $(function() {
  //        });
  //    }
 
-    login();
 
-    function loginReload() {
-        $.getJSON("http://localhost:8080/api/games", function(data) {
-            if(data.player != null) {
-                showOutput("Logged in as: " + data.player.playerName);
-                $("#login-form").hide();
-            }
-            var games = data.games;
-            if(data.player != null) {
-                for (var i = 0; i < games.length; i++) {
-                    games[i].gameplayers.map(function (x) {
-                        if (x.player["playerId"] == data.player["playerId"]) {
-                            $("#dataAttr" + i).append("<button class='buttonsRejoin'" + " onclick=window.location.href='http://localhost:8080/web/game.html?gp=" + x.id + "'>Go to your game</button>")
-                        }
-                    });
+
+function loginReload() {
+    $.getJSON("/api/games", function(data) {
+    if(data.player != null) {
+        showOutput("Logged in as: " + data.player.playerName);
+        $("#login-form").hide();
+    }
+    var games = data.games;
+    if(data.player != null) {
+        for (var i = 0; i < games.length; i++) {
+            games[i].gameplayers.map(function (x) {
+                if (x.player["playerId"] == data.player["playerId"]) {
+                    $("#dataAttr" + i).append("<button class='buttonsRejoin'" + " onclick=window.location.href='http://localhost:8080/web/game.html?gp=" + x.id + "'>Go to your game</button>")
                 }
-            }
-
-
-        });
-    }
-
-    loginReload();
-
-    function logout() {
-        $("#logoutButton").click(function(event) {
-            event.preventDefault();
-            $.post("/api/logout")
-                .done(function () {
-                    console.log("Logged out")
-                    showOutput("");
-                    $("#login-form").show();
-                    $(".buttonsRejoin").hide();
-                    $(".buttonJoinGame").hide();
-                })
-                .fail();
-        });
-    }
-
-    logout();
-
-    function signUp() {
-        $("#signupButton").click(function(event) {
-            event.preventDefault();
-            var form = event.target.form;
-            $.post("/api/players",
-                { username: form["username"].value,
-                    password: form["password"].value })
-                .done(function( ) {
-                    console.log("You are signed up" )
-                    loginPost(form);
-                    // loadNewPlayer();
-                })
-                .fail(function( jqXHR, textStatus ) {
-                    showOutput("Error!");
-                });
-        });
-    }
-
-    signUp();
-
-    function createGame() {
-        $("#createGameButton").click(function(event) {
-            event.preventDefault();
-            $.post("/api/games")
-                .done(function (data) {
-                    console.log("GamePlayerId " + data.gamePlayerId);
-                    window.location.href='http://localhost:8080/web/game.html?gp=' + data.gamePlayerId;
-                })
-                .fail(
-                    console.log("You failed!")
-                );
-        });
-    }
-
-    createGame();
-
-    function joinButton() {
-        $.getJSON("http://localhost:8080/api/games", function(data) {
-            data.games.forEach(function(element, i) {
-                element.gameplayers.forEach(function(x) {
-                    if(element.gameplayers.length ==  1 && data.player.playerId != x.player.playerId) {
-                        $("#dataAttr" + i).append("<button class='buttonJoinGame' data-joinButton="+ element.id + ">" + "Join Game" + "</button>");
-                    }
-                });
-
             });
-            joinGame();
-        });
+        }
     }
+    });
+}
 
-    joinButton();
+function logout() {
+$("#logoutButton").click(function(event) {
+    event.preventDefault();
+    $.post("/api/logout")
+        .done(function () {
+            console.log("Logged out")
+            showOutput("");
+            $("#login-form").show();
+            $(".buttonsRejoin").hide();
+            $(".buttonJoinGame").hide();
+        })
+        .fail();
+    });
+}
 
-    function joinGame() {
-        $(".buttonJoinGame").click(function(event) {
-            event.preventDefault();
-            var id = event.target.attributes[1].value;
-            console.log(id);
-            $.post("/api/game/"+id+"/players")
-                .done(function(data) {
-                    console.log(data);
-                    window.location.href='http://localhost:8080/web/game.html?gp=' + data.gamePlayerId;
-                })
-                .fail();
+
+
+function signUp() {
+    $("#signupButton").click(function(event) {
+        event.preventDefault();
+        var form = event.target.form;
+        $.post("/api/players",
+            { username: form["username"].value,
+                password: form["password"].value })
+            .done(function( ) {
+                console.log("You are signed up" )
+                loginPost(form);
+                // loadNewPlayer();
+            })
+            .fail(function( jqXHR, textStatus ) {
+                showOutput("Error!");
+            });
+    });
+}
+
+
+
+function createGame() {
+    $("#createGameButton").click(function(event) {
+        event.preventDefault();
+        $.post("/api/games")
+            .done(function (data) {
+                console.log("GamePlayerId " + data.gamePlayerId);
+                window.location.href='/web/game.html?gp=' + data.gamePlayerId;
+            })
+            .fail(
+                console.log("You failed!")
+            );
+    });
+}
+
+
+
+function joinButton(data) {
+    $.getJSON("/api/games", function(data) {
+
+        var mainPlayer = data.player.playerId;
+        data.games.forEach(function (element, i) {
+            element.gameplayers.forEach(function (x) {
+                if (element.gameplayers.length == 1 && mainPlayer != x.player.playerId) {
+                    $("#dataAttr" + i).append("<button class='buttonJoinGame' data-joinButton=" + element.id + ">" + "Join Game" + "</button>");
+                }
+            });
         });
-    }
+        joinGame();
+    });
 
-    // $.ajax({
-    //     method: "POST",
-    //     contentType: "application/json; charset=utf-8",
-    //     url: "http://localhost:8080/api/games/players/15/ships",
-    //     data: JSON.stringify([{ "shipType": "destroyer", "shipLocation": ["A1", "B1", "C1"]},{ "shipType": "patrol boat", "shipLocation": ["H5", "H6"]}])
-    // })
-});
+
+}
+
+
+
+function joinGame() {
+    $(".buttonJoinGame").click(function(event) {
+        event.preventDefault();
+        var id = event.target.attributes[1].value;
+        console.log(id);
+        $.post("/api/game/"+id+"/players")
+            .done(function(data) {
+                console.log(data);
+                window.location.href='/web/game.html?gp=' + data.gamePlayerId;
+            })
+            .fail();
+    });
+}
+
+function allGets() {
+    $.getJSON("/api/games", function(data_json) {
+        var data = data_json;
+        makeLeaderBoard(data);
+        loginReload(data);
+        joinButton(data);
+        console.log(data);
+
+    });
+}
+
+// $.ajax({
+//     method: "POST",
+//     contentType: "application/json; charset=utf-8",
+//     url: "http://localhost:8080/api/games/players/15/ships",
+//     data: JSON.stringify([{ "shipType": "destroyer", "shipLocation": ["A1", "B1", "C1"]},{ "shipType": "patrol boat", "shipLocation": ["H5", "H6"]}])
+// })
